7531b98906c5350ceaf2e287cc3bc85749c17a42
Lucas Fernandes <lfaa2057@gmail.com>
2024-05-10 20:07:00 -0300

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let counter = 0
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    counter++;
+    res.render('vies', { counter });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
